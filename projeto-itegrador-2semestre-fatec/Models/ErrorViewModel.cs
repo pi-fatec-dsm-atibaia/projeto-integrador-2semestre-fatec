b925b3627140b430c88c333bb0984e1772173716
Christian Andrade <andrade.dias.christian@gmail.com>
2025-07-04 19:59:36 -0300

    Adicionar arquivos de projeto.

---
+++ projeto-itegrador-2semestre-fatec/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace projeto_itegrador_2semestre_fatec.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
