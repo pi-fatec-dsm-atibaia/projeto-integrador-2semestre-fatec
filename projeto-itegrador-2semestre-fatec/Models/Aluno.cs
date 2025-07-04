84b4eb58364b7c0a5f09283c2e03577fc32138fc
Iago Homsi <iagohomsi@gmail.com>
2025-07-04 20:37:18 -0300

    Arquivo da MODEL aluno preenchido

--- projeto-itegrador-2semestre-fatec/Models/Aluno.cs
+++ projeto-itegrador-2semestre-fatec/Models/Aluno.cs
@@ -1,6 +1,35 @@
-﻿namespace projeto_itegrador_2semestre_fatec.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projeto_itegrador_2semestre_fatec.Models
 {
     public class Aluno
     {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public Curso curso { get; set; }
+
+        [Required]
+        public string ra { get; set; }
+
+        [Required]
+        public string rg { get; set; }
+
+        [Required]
+        public string telefone { get; set; }
+
+        [Required]
+        public string nome { get; set; }
+
+        [Required]
+        public string email { get; set; }
+
+        [Required]
+        public string cpf { get; set; }
+
+        [Required]
+        public string senha { get; set; }
+
     }
 }
