5d007c21a77a83e19efb77cff5ce491803f0e1d6
Alex Araujo <pokekiteria97@gmail.com>
2025-07-04 21:18:21 -0300

    create model curso

--- projeto-itegrador-2semestre-fatec/Models/Curso.cs
+++ projeto-itegrador-2semestre-fatec/Models/Curso.cs
@@ -1,6 +1,22 @@
-﻿namespace projeto_itegrador_2semestre_fatec.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace projeto_itegrador_2semestre_fatec.Models
 {
     public class Curso
     {
+        [Required]
+        public int id { get; set; }
+
+        [Required]
+        public  string nome { get; set; }   
+
+        [Required]
+        public int quantSemestre { get; set; }
+         
+        [Required]
+        public string periodo { get; set; }
+
+
+
     }
 }
