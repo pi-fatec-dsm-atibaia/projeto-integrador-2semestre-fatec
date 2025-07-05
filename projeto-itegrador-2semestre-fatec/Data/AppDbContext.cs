171b57c9ef3bc90cc227f0a896ff68f9ab9b2702
Iago Homsi <iagohomsi@gmail.com>
2025-07-04 21:37:16 -0300

    Criação de VIEW da página LOGIN

--- projeto-itegrador-2semestre-fatec/Data/AppDbContext.cs
+++ projeto-itegrador-2semestre-fatec/Data/AppDbContext.cs
@@ -1,6 +1,12 @@
-﻿namespace projeto_itegrador_2semestre_fatec.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projeto_itegrador_2semestre_fatec.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public Dbset<Aluno> Aluno { get; set; }
+
     }
 }
--- projeto-itegrador-2semestre-fatec/Program.cs
+++ projeto-itegrador-2semestre-fatec/Program.cs
@@ -1,7 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using projeto_itegrador_2semestre_fatec.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseMySql(builder.Configuration.GetConnectionString(
+    "DefaultConnection"), new MySqlServerVersion(
+        new Version(8, 0, 33))));
 
 var app = builder.Build();
 
