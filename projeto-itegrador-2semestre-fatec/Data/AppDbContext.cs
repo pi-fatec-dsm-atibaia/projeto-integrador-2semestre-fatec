7c243f3f419cc8d671c79e0f3a1fbba438a7a9cb
Christian Andrade <andrade.dias.christian@gmail.com>
2025-07-07 20:22:33 -0300

    reset model

--- projeto-itegrador-2semestre-fatec/Data/AppDbContext.cs
+++ projeto-itegrador-2semestre-fatec/Data/AppDbContext.cs
@@ -1,6 +1,9 @@
-﻿namespace projeto_itegrador_2semestre_fatec.Data
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace projeto_itegrador_2semestre_fatec.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
     }
 }
